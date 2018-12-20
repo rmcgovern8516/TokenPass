205f1a5851bf5147abee0d4f81cdc6f3c3ae8f75
BT_1N3_32 <rmcgovern8516@bths.edu>
2018-12-20 08:31:25 -0500

    Final Version

--- src/RyanMcGovern/TokenPass.java
+++ src/RyanMcGovern/TokenPass.java
@@ -5,7 +5,8 @@ public class TokenPass {
     private int currentPlayer;
 
 
-    public void TokenPass(int playerCount){
+    public TokenPass(int playerCount)
+    {
         board = new int[playerCount];
         this.currentPlayer = (int)(Math.random()*playerCount);
         for (int i=0; i<playerCount; i++){
@@ -18,10 +19,27 @@ public class TokenPass {
     }
 
     public int getCurrentPlayer() {
-        return currentPlayer;
+        return currentPlayer+1;
     }
 
-    public void distributeCurrentPlayerTokens(){
-        
+    public void distributeCurrentPlayerTokens()
+    {
+        int currentTokens = board[currentPlayer];
+        board[currentPlayer] = 0;
+        while (currentTokens>0){
+            for (int i=0; i<board.length; i++)
+            {
+                if (currentTokens>0)
+                {
+                    board[i]++;
+                    currentTokens--;
+                }
+                else
+                {
+                    i = board.length;
+                }
+            }
+
+        }
     }
 }
--- src/RyanMcGovern/TokenPassRunner.java
+++ src/RyanMcGovern/TokenPassRunner.java
@@ -4,18 +4,18 @@ public class TokenPassRunner {
 
     public static void main(String[] args) {
 	// write your code here
-        TokenPass testGame = new TokenPass();
+        TokenPass testGame = new TokenPass(5);
         int[] testBoard = testGame.getBoard();
         System.out.println("Constructed a testBoard: ");
         for (int i=0; i<testBoard.length; i++){
-            System.out.println(testBoard[i]+" ");
+            System.out.print(testBoard[i]+" ");
         }
         System.out.println();
 
         testGame.distributeCurrentPlayerTokens();
         System.out.println("After distributing tokens from player "+testGame.getCurrentPlayer()+": ");
         for (int i=0; i<testBoard.length; i++){
-            System.out.println(testBoard[i]+" ");
+            System.out.print(testBoard[i]+" ");
         }
     }
 }
